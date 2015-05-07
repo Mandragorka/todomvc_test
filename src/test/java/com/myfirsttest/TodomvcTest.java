5d92b28c8a31c2a3e4c4cf337b246138f4c9a0be
Mandragorka <mandragora.olenka@gmail.com>
2015-05-07 15:49:20 -0700

    Added some changes, again

--- src/test/java/com/myfirsttest/TodomvcTest.java
+++ src/test/java/com/myfirsttest/TodomvcTest.java
@@ -1,5 +1,7 @@
 package com.myfirsttest;
 
+        import com.codeborne.selenide.ElementsCollection;
+        import com.codeborne.selenide.SelenideElement;
         import org.junit.Test;
 
         import java.util.List;
@@ -13,10 +15,10 @@ public class TodomvcTest {
 
     @Test
     public void enterTasks() {
-        String todoListSelector = ("#todo-list li");
-        String filterSelector = ("#filters a");
-        String activeTaskSelector = ("#todo-count");
-        String completedTaskSelector = ("#clear-completed");
+        ElementsCollection tasks = $$("#todo-list li");
+        ElementsCollection filters = $$("#filters a");
+        SelenideElement activeCount = $("#todo-count");
+        SelenideElement clearCompleted = $("#clear-completed");
 
         String t1 = "1. Practice kindness";
         String t2 = "=(^.^)=";
@@ -30,76 +32,73 @@ public class TodomvcTest {
             $("#new-todo").val(t).pressEnter();
         }
 
-        $$(todoListSelector).shouldHave(exactTexts(t1, t2, t3, t4));
+        tasks.shouldHave(exactTexts(t1, t2, t3, t4));
 
         // Перевіряю одразу кількість активних тасок
-        $(activeTaskSelector).shouldHave(text("4"));
-        // Потім перевірка для clear-completed коли знаходимось під фільтром All
-        $$(filterSelector).find(text("All")).shouldHave(cssClass("selected"));
-        $(completedTaskSelector).shouldBe(hidden).shouldHave("0"); //???
+        activeCount.shouldHave(text("4"));
 
         // Delete second task
-        $$(todoListSelector).get(1).hover();
-        $$(todoListSelector).get(1).find(".destroy").click();
+        tasks.get(1).hover();
+        tasks.get(1).find(".destroy").click();
 
         // Make sure that task was deleted
-        $$(todoListSelector).get(1).shouldNotHave(text(t2));
+        tasks.get(1).shouldNotHave(text(t2));
         // Checks count of active items after deleting 2nd task
-        $(activeTaskSelector).shouldHave(text("3"));
+        activeCount.shouldHave(text("3"));
 
         // Mark 4th task as a completed
-        $$(todoListSelector).get(2).shouldHave(text(t4)).find(".toggle").click();
+        tasks.get(2).shouldHave(text(t4)).find(".toggle").click();
 
         // Active filter verification
-        $$(filterSelector).find(text("Active")).click();
-        $$(filterSelector).find(text("Active")).shouldHave(cssClass("selected"));
+        filters.find(text("Active")).click();
+        filters.find(text("Active")).shouldHave(cssClass("selected"));
         $(".completed").shouldBe(hidden);
         $$(".active").shouldHaveSize(2);
 
         // Completed filter verification
-        $$(filterSelector).find(text("Completed")).click();
-        $$(filterSelector).find(text("Completed")).shouldHave(cssClass("selected"));
+        filters.find(text("Completed")).click();
+        filters.find(text("Completed")).shouldHave(cssClass("selected"));
         $(".active").shouldBe(hidden);
         $$(".completed").shouldHaveSize(1);
 
         // Back to All filter
-        $$(filterSelector).find(text("All")).click();
-        $$(filterSelector).find(text("All")).shouldHave(cssClass("selected"));
+        filters.find(text("All")).click();
+        filters.find(text("All")).shouldHave(cssClass("selected"));
 
         // Checks count of active items
-        $(activeTaskSelector).shouldHave(text("2"));
+        activeCount.shouldHave(text("2"));
         // Checks count of completed items
-        $(completedTaskSelector).shouldHave("1");
+        clearCompleted.shouldHave("1");
 
         // Clear 4th task
-        $(completedTaskSelector).click();
+        clearCompleted.click();
 
         // Checks clear-completed after deleting completed tasks
-        $$(filterSelector).find(text("All")).has(cssClass("selected"));
-        $(completedTaskSelector).shouldBe(hidden).shouldHave("0");
+        filters.find(text("All")).has(cssClass("selected"));
+        clearCompleted.shouldBe(hidden);
 
         // Make sure that 4th task was deleted
-        $$(todoListSelector).shouldHaveSize(2);
+        tasks.shouldHaveSize(2);
 
         // Task editing - appending
-        actions().doubleClick($$(todoListSelector).get(1).find("label")).perform();
-        $$(todoListSelector).get(1).find(".edit").append("appended").pressEnter();
-        $$(todoListSelector).get(1).shouldHave(text("appended"));
+        actions().doubleClick(tasks.get(1).find("label")).perform();
+        tasks.get(1).find(".edit").append("appended").pressEnter();
+        tasks.get(1).shouldHave(text("appended"));
 
         // Task editing - replacing
-        actions().doubleClick($$(todoListSelector).get(0).find("label")).perform();
-        $$(todoListSelector).get(0).find(".edit").val("new task").pressEnter();
-        $$(todoListSelector).get(0).shouldHave(text("new task"));
+        actions().doubleClick(tasks.get(0).find("label")).perform();
+        tasks.get(0).find(".edit").val("new task").pressEnter();
+        tasks.get(0).shouldHave(text("new task"));
 
         // Mark ALL tasks as completed
         $("#toggle-all").click();
 
         // Checks count of completed items
-        $(completedTaskSelector).shouldHave("2");
+        clearCompleted.shouldHave("2");
 
         // Clear all completed tasks
-        $(completedTaskSelector).click();
+        clearCompleted.click();
 
-        $$(todoListSelector).shouldHaveSize(0);
+        tasks.shouldHaveSize(0);
     }
 }
